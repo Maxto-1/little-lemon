80615b7369040834a8a755753c9953838598d8de
Maxto-1 <kaououed998@gmail.com>
2025-08-22 00:08:28 +0000

    Correction de l'import os dans settings.py

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for littlelemon project.
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-!*^&4n9@8%^+nfa+brg5*z=*_ip_bnh3#t=r%^*v17o(f=)h@1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'littlelemon.urls'
@@ -124,7 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
